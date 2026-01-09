82e31c5b154219cb411fb9ec510a46d4ff17f84f
danisankovich <misankovich@gmail.com>
2026-01-09 00:45:50 -0500

    change to typescript

---
+++ src/index.ts
@@ -0,0 +1,142 @@
+function generateColumns(): void {
+    const widthInch: number = screen.width / 96;
+    const heightInch: number = screen.height / 96;
+    const GRID: HTMLElement | null = document.getElementById('grid');
+    if (GRID) {
+        for (let counterHeight = 0; counterHeight < heightInch; counterHeight++) {
+            const ROW = document.createElement('div');
+            for (let counterWidth = 0; counterWidth < widthInch; counterWidth++) {
+                const CELL = document.createElement('div');
+                CELL.ondrop = dropHandler;
+                CELL.ondragover = dragOverHandler;
+                ROW.appendChild(CELL).className = 'cell';
+            }
+            GRID.appendChild(ROW).className = 'cell-row';
+        }
+    }
+}
+
+generateColumns();
+
+async function fetchBattleMat(name: string) {
+    try {
+        const response = await fetch(`./battle_mats/${name}/settings.json`, { mode: 'no-cors'});
+        const data = await response.json();
+        return data;
+    } catch (err: any) {
+        throw new Error(err)
+    }
+}
+
+function addAssetToScreen(asset: any, battleMatName: string) {
+
+    // {
+    //         "name": "goblin",
+    //         "id": 1,
+    //         "type": "creature",
+    //         "subtype": "hostile",
+    //         "path": "goblin.jpg",
+    //         "size": "medium",
+    //         "startingXCell": 0,
+    //         "startingYCell": 0
+    //     }
+
+    const { name, id, type, subtype, path, size, startingXCell, startingYCell } = asset;
+    const GRID: HTMLElement | null = document.getElementById('grid');
+    
+    if (GRID) {
+        const IMG_CONTAINER = document.createElement('div');
+        const IMG = document.createElement('img');
+        
+        IMG.src = `./battle_mats/${battleMatName}/assets/${path}`;
+        IMG.classList.add('asset', type);
+        if (subtype) {
+            IMG.classList.add(subtype);
+        }
+
+        if (['tiny', 'small', 'medium', 'large', 'huge', 'gargantuan'].includes(size)) {
+            IMG.classList.add(size);
+            IMG_CONTAINER.classList.add(`image-container-${size}`);
+        } else {
+            IMG_CONTAINER.classList.add(`image-container`);
+        }
+
+        if (startingXCell) {
+            IMG.style.left = `${((startingXCell - 1) * 96) + 1}px`;
+        }
+
+        if (startingYCell) {
+            IMG.style.top = `${((startingYCell - 1) * 96) + 12}px`;
+        }
+        IMG.draggable = true;
+        IMG.ondragstart = dragstartHandler;
+        IMG.id = `${name}-${id}`; // give unique IDs in the JSON when generating these for targeting reasons.
+        GRID.appendChild(IMG);
+    }
+}
+
+// addAssetToScreen('goblin.jpg', 3, 2);
+
+function placeAssets(assets: any, battleMatName: string) {
+    assets.forEach((asset: any) => {
+        addAssetToScreen(asset, battleMatName);
+    })
+}
+
+async function initializeMap(name: string) {
+    const battleMat = await fetchBattleMat(name);
+    placeAssets(battleMat.assets, name)
+}
+
+initializeMap('default').then(data=> {
+    console.log(data);
+})
+
+function dragstartHandler(event: any) {
+    const rect = event.target.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left;
+    const offsetY = event.clientY - rect.top;
+    console.log(offsetX, offsetY);
+    event.dataTransfer.setData("text/plain", `${event.target.id},${offsetX},${offsetY}`);    
+}
+
+function dragOverHandler(event: Event) {
+      event.preventDefault();
+}
+
+// function snapToPosition(value) {
+//     const inch = 96;
+//     if (value % inch <= 48) {
+//         return value - (value % inch);
+//     }
+//     return value + (96 - value % inch)
+// }
+
+function dropHandler(event: any) {
+    event.preventDefault();
+
+    const data = event.dataTransfer.getData("text/plain").split(',');
+    const id = data[0];
+    const dropOffsetX = parseInt(data[1], 10);
+    const dropOffsetY = parseInt(data[2], 10);
+
+    const drop_item: HTMLElement | null = document.getElementById(id);
+    if (drop_item) {
+        drop_item.style.left = `${event.clientX - dropOffsetX + window.scrollX}px`;
+        drop_item.style.top = `${event.clientY - dropOffsetY + window.scrollY}px`;
+    }
+}
+
+
+// TODO
+// run app via node, so that it can react to terminal commands
+// add tokens to screen via terminal command --> maybe with sockets
+// maybe electron app?
+
+// alternatively, just have assets to load up, and depending on the assets, you tell it
+// what to add to screen and where to begin. So like, each battlemap has an image (the grid)
+// and a json, which includes enemy types, enemy numbers, and coordinates. 
+// This can also be used for assets like trees and such, for when you don't
+// have stylized backgrounds. You just have a piece of UI at the top with
+// a dropdown to load the desired assets.
+// or maybe you open the dropdown with a keypress.
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 1. Require the plugin
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  // Set the mode (development or production)
+  mode: 'development', 
+  // Define the entry point of your application
+  entry: './src/index.ts',
+  // Configure the output bundle
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Output to a 'dist' folder
+  },
+  // Define rules for handling different file types
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // Test for .ts or .tsx files
+        use: 'ts-loader', // Use ts-loader to compile them
+        exclude: /node_modules/, // Exclude node_modules
+      },
+    //   {
+    //     test: /\.css$/i, // Regex to match .css files
+    //     use: [
+    //       MiniCssExtractPlugin.loader, // Use MiniCssExtractPlugin loader for production build
+    //       'css-loader', // Handles CSS imports
+    //     ],
+    //   },
+    ],
+  },
+  // Resolve file extensions
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  // Add source maps for easier debugging in the browser
+  devtool: 'inline-source-map',
+  // Configure the development server
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // 2. Specify the source HTML template file
+      title: 'Tabletop Manager', // Optional: customize the HTML title tag
+    }),
+    // new MiniCssExtractPlugin({
+    //   filename: '[name].css', // Output path and filename for the CSS file in dist
+    // }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/battle_mats', // Source glob pattern
+          to: 'battle_mats',         // Destination in the dist folder (e.g., dist/json/data.json)
+        }, {
+          from: 'src/styles', // Source glob pattern
+          to: 'styles',         // Destination in the dist folder (e.g., dist/json/data.json)
+        },
+      ],
+    })
+  ]
+};
