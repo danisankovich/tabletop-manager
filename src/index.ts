1c2e535da1efeddb2e139d355b62951d51a3fee1
danisankovich <misankovich@gmail.com>
2026-01-09 02:17:34 -0500

    add readmen. but it needs actual info

--- src/index.ts
+++ src/index.ts
@@ -144,9 +144,3 @@ function dropHandler(event: any) {
     }
 
 }
-
-
-// TODO
-// a dropdown to load the desired battlemaps. maybe opened with a keypress combo.
-// a dropdown to add assets listed in a battlemap folder. For example, if a tree or dragon
-// needs to be added mid-fight, there should be a means to do this.
