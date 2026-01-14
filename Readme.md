9f87802f584926a13ee47e6c019782a32750dbca
danisankovich <misankovich@gmail.com>
2026-01-14 13:23:21 -0500

    updates

--- Readme.md
+++ Readme.md
@@ -1,3 +1,13 @@
+a simple web app to be used with a digital tabletop rpg display.
+
 TODO
  - a dropdown to load the desired battlemaps. maybe opened with a keypress combo.
  - a dropdown to add assets listed in a battlemap folder. For example, if a tree or dragon needs to be added mid-fight, there should be a means to do this.
+ - all enemy assets should have their ID on their tile
+ - Be able to right click an asset to open an options menu
+    - enlarge/shrink
+    - dead
+    - status conditions
+    - difficult terrain marker
+    - charmed
+    - etc
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import type { IBattleMapAsset, IBattleMap } from './types.ts';
 
+const CELL_INCH = 92;
+
+let CLICKED_TOKEN: HTMLElement | null;
+let SHOW_MENU: boolean = false;
+
 function generateColumns(): void {
     const widthInch: number = screen.width / 96;
     const heightInch: number = screen.height / 96;
@@ -16,6 +21,17 @@ function generateColumns(): void {
             }
             GRID.appendChild(ROW).className = 'cell-row';
         }
+    }
+
+    const DRAGGABLE_GRID: HTMLElement | null = document.getElementById('draggable-grid');
+    if (DRAGGABLE_GRID) {
+        DRAGGABLE_GRID.addEventListener('offclic', function() {
+            // console.log('ahhh')
+            // CLICKED_TOKEN = null;
+            // SHOW_MENU = false;
+            // closeMenu();
+            // currently doesn't work and breaks opening the menu
+        })
     }
 }
 
@@ -50,12 +66,47 @@ function addAssetToScreen(asset: IBattleMapAsset, battleMapName: string) {
             IMG_CONTAINER.classList.add('hostile');
         }
 
-        if (['tiny', 'small', 'medium', 'large', 'huge', 'gargantuan'].includes(size)) {
-            IMG.classList.add(size);
-            IMG_CONTAINER.classList.add(`image-container-${size}`);
-        } else {
-            IMG_CONTAINER.classList.add(`image-container`);
+        IMG_CONTAINER.classList.add(`image-container-${size}`);
+        let width, height;
+        switch(size.toLowerCase()) {
+            case 'tiny':
+                width = CELL_INCH;
+                height = CELL_INCH;
+            case 'small':
+                width = CELL_INCH;
+                height = CELL_INCH;
+            case 'medium':
+                width = CELL_INCH;
+                height = CELL_INCH;
+            case 'large':
+                width = 2 * CELL_INCH;
+                height = 2 * CELL_INCH;
+            case 'huge':
+                width = 3 * CELL_INCH;
+                height = 3 * CELL_INCH;
+            case 'gargantuan':
+                width = 4 * CELL_INCH;
+                height = 4 * CELL_INCH;
+            default:
+                const measurements = size.toLowerCase().split('x');
+                if (measurements.length !== 2) {
+                    width = CELL_INCH;
+                    height = CELL_INCH;
+                } else {
+                    // @ts-ignore
+                    if (measurements[0] && !isNaN(measurements[0]) && measurements[1] && !isNaN(measurements[1])) {
+                        width = parseInt(measurements[0], 10) * CELL_INCH;
+                        height = parseInt(measurements[1], 10) * CELL_INCH;
+                    } else {
+                        width = CELL_INCH;
+                        height = CELL_INCH;
+                    }
+                }
         }
+
+        IMG_CONTAINER.style.height = `${height}px`;
+        IMG_CONTAINER.style.width = `${width}px`;
+        IMG.classList.add('token-image');
 
         if (startingXCell) {
             IMG_CONTAINER.style.left = `${((startingXCell - 1) * 96) + 1}px`;
@@ -71,17 +122,17 @@ function addAssetToScreen(asset: IBattleMapAsset, battleMapName: string) {
         IMG_CONTAINER.ondragstart = dragstartHandler;
         IMG_CONTAINER.id = `${name}-${id}`; // give unique IDs in the JSON when generating these for targeting reasons.
         IMG_CONTAINER.appendChild(IMG);
-        if (IMG.draggable) {
+        if (IMG_CONTAINER.draggable) {
             DRAGGABLE_GRID.appendChild(IMG_CONTAINER);
         } else {
             GRID.appendChild(IMG_CONTAINER);
         }
     }
+    addEventListenersToAsset(asset);
 }
 
 function placeAssets(battleMap: IBattleMap, battleMapName: string) {
     if (battleMap.battle_map_source) {
-        console.log('here?')
         const GRID: HTMLElement | null = document.getElementById('grid');
         if (GRID) {
             const backgroundSource = `./battle_maps/${battleMapName}/assets/${battleMap.battle_map_source}`;
@@ -93,15 +144,15 @@ function placeAssets(battleMap: IBattleMap, battleMapName: string) {
 
     battleMap.assets.forEach((asset: IBattleMapAsset) => {
         addAssetToScreen(asset, battleMapName);
-    })
+    });
 }
 
-async function initializeMap(name: string) {
+async function initializeMap(name: string = 'default') {
     const battleMap = await fetchBattleMap(name);
     placeAssets(battleMap, name)
 }
 
-initializeMap('field').then(data=> {
+initializeMap('default').then(data=> {
     console.log('initialized');
 })
 
@@ -139,8 +190,65 @@ function dropHandler(event: any) {
     if (drop_item) {
         drop_item.style.left = `${event.clientX - dropOffsetX + window.scrollX}px`;
         drop_item.style.top = `${event.clientY - dropOffsetY + window.scrollY}px`;
-        console.log(drop_item)
         DRAGGABLE_GRID?.appendChild(drop_item);
-    }
+    }    
+}
 
+// menu functions
+
+function addEventListenersToAsset(asset: IBattleMapAsset) {
+    if (asset) {
+        const ID = `${asset.name}-${asset.id}`;
+        const assetElement: HTMLElement | null = document.getElementById(ID);
+        if (assetElement) {
+            assetElement.addEventListener('mouseup', function(event: MouseEvent) {
+                CLICKED_TOKEN = assetElement;
+                openMenu(event, ID);
+            });
+        }
+    }
+}
+
+function openMenu(event: MouseEvent, ID: string) {
+    const DRAGGABLE_GRID = document.getElementById('draggable-grid');
+    const { clientX, clientY } = event;
+
+    closeMenu();
+
+    SHOW_MENU = true;
+    const menu = document.createElement('div');
+    menu.id = 'token-menu';
+
+    menu.style.left = clientX + 168 > screen.width ? `${clientX - 168}px` : `${clientX}px`;
+    menu.style.top = clientY + 200 > screen.width ? `${clientY - 200}px` : `${clientY}px`;
+
+    const removeButton = document.createElement('button');
+    removeButton.onclick = removeToken;
+
+    menu.appendChild(removeButton);
+    DRAGGABLE_GRID?.appendChild(menu);
+}
+
+function closeMenu() {
+    const DRAGGABLE_GRID = document.getElementById('draggable-grid');
+    SHOW_MENU = false;
+    const menuToRemove = document.getElementById('token-menu');
+    if (menuToRemove) {
+        DRAGGABLE_GRID?.removeChild(menuToRemove);
+    }
+}
+
+function removeToken() {
+    const GRID: HTMLElement | null | undefined = CLICKED_TOKEN?.parentElement;
+    if (CLICKED_TOKEN) {
+        GRID?.removeChild(CLICKED_TOKEN);
+    }
+}
+
+function setCondition() {
+    // for all of these, you should build a template. instead of building the menu by scratch every time
+    // build it/copy it from that template. Since we are storing the clicked element and we can
+    // find which grid it is on by searching for the parent, that should do it.
+
+    // this one needs a secondary menu to pop up.
 }
